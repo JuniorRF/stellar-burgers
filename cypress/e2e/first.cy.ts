66cf81a25e2e1054581ae47a3aba4ba744f890f2
unknown <demchakromka@gmail.com>
2025-04-25 20:35:41 +0300

    фикстуры ингредиентов и пользака

--- cypress/e2e/first.cy.ts
+++ cypress/e2e/first.cy.ts
@@ -1,5 +1,12 @@
 describe('проверяем доступность приложения', function() {
+    beforeEach(() => {
+        cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients' });
+        cy.intercept("GET", "api/auth/user", { fixture: "user",});
+    })
     it('сервис должен быть доступен по адресу localhost:4000', function() {
-        cy.visit('http://localhost:4000'); 
+        cy.visit('http://localhost:4000');
+    });
+    it('открывает модльные окна', function() {
+        cy.get('');
     });
 }); 
